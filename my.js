d22ac5ef4509c0c2580c728cf7d3fa337f1391b5
NikitaGordievskiy <113328295+NikitaGordievskiy@users.noreply.github.com>
2022-12-19 09:23:41 +0300

    Update my.js

--- my.js
+++ my.js
@@ -18,3 +18,34 @@ $(function(){
         });
     });
 });
+$(document).on('ready', function() {
+    $('.worked_with_us_slider1').slick({
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
+  
+  $(document).on('ready', function() {
+    $('.worked_with_us_slider2').slick({
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
+  
+  
+  $(document).on('ready', function() {
+    $('.rewiews_slider').slick({
+      infinite: true,
+      adaptiveHeight: true,
+      fade: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+  
+    });
+  });
